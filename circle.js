62edf4a2cdc38a7ee225e0b643526a33fd8dccf6
sachin000012 <116427676+sachin000012@users.noreply.github.com>
2023-01-15 19:14:30 +0530

    first commit

---
+++ circle.js
@@ -0,0 +1,14 @@
+var Shape = require("./shape");
+
+// declare class
+class Circle extends Shape {
+  constructor(name) {
+    super(name);
+  }
+
+  calculateArea(radius) {
+    return Math.PI * Math.pow(radius, 2);
+  }
+}
+
+module.exports = Circle;
